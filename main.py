baa60c8fbd283dc6b72a8984bfddd264edbc1105
Krendelev <krendelev@gmail.com>
2019-09-06 01:14:59 +0300

    use tempfile

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-import io
 import os
 import random
-import shutil
-from urllib.parse import urlsplit
+import tempfile
 
 import requests
 from dotenv import load_dotenv
 
 
 def get_comics_number():
-    current_number = get_comics()[0]
+    current_number = get_comics()[2]
     return random.randint(1, current_number + 1)
 
 
@@ -18,7 +16,7 @@ def get_comics(comics_number=None):
     response = requests.get(f"http://xkcd.com/{comics_number}/info.0.json")
     response.raise_for_status()
     content = response.json()
-    return content["num"], content["alt"], content["img"]
+    return content["img"], content["alt"], content["num"]
 
 
 def get_image(url):
@@ -27,10 +25,8 @@ def get_image(url):
     return response.content
 
 
-def save_image(image, filename):
-    with open(filename, "wb") as fh:
-        fh.write(image)
-    return None
+def get_image_extension(url):
+    return url.split(".")[-1]
 
 
 def check_vk_response(response):
@@ -47,8 +43,8 @@ def get_upload_url(payload):
     return response["response"]["upload_url"]
 
 
-def upload_picture(url, file_path):
-    files = {"photo": open(file_path, "rb")}
+def upload_picture(url, image):
+    files = {"photo": image}
     response = requests.post(url, files=files).json()
     check_vk_response(response)
     return response
@@ -89,25 +85,24 @@ if __name__ == "__main__":
     load_dotenv()
 
     try:
-        _, caption, url = get_comics(get_comics_number())
+        url, caption, _ = get_comics(get_comics_number())
         image = get_image(url)
     except requests.HTTPError as error:
         exit(error)
 
-    file_name = os.path.basename(urlsplit(url).path)
-    dir_name = "image"
-    os.makedirs(dir_name)
-    file_path = os.path.join(dir_name, file_name)
-    save_image(image, file_path)
+    extension = get_image_extension(url)
+    temp = tempfile.NamedTemporaryFile(suffix=f".{extension}")
+    temp.write(image)
+    temp.seek(0)
 
     payload = {"access_token": os.environ["VK_ACCESS_TOKEN"], "v": 5.101}
 
     try:
         upload_url = get_upload_url(payload)
-        upload_info = upload_picture(upload_url, file_path)
+        upload_info = upload_picture(upload_url, temp)
         picture_info = save_picture(payload, upload_info)
         post_picture(payload, picture_info, caption)
     except requests.HTTPError as error:
         print(error)
     finally:
-        shutil.rmtree(dir_name)
+        temp.close()
